d9e3021cc23627d2d9e68d115cce719f5bd0929c
Dan <42252536+JustEz7z@users.noreply.github.com>
2019-06-09 22:16:46 +0300

    Did

--- СodeWar/codeWarT2.py
+++ СodeWar/codeWarT2.py
@@ -1,5 +1,7 @@
 def reverse(st):
-    str = []
-    for i in st:
-        
-reverse("Hello World!")
+    st = st.split()
+    st.reverse()
+    st = " ".join(st)
+    return st
+reverse("Hello World")
+reverse("Hi There.")
